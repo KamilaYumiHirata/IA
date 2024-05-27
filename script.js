123b96ed1c80c5ef7c7f0fee7a6b557f0f1e4752
KamiYumi <111126080+KamilaYumiHirata@users.noreply.github.com>
2024-05-27 11:39:54 -0300

    aaa

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
 const perguntas = [
     {
         enunciado: "Pergunta 1",
@@ -13,4 +19,26 @@ const perguntas = [
             "Alternativa 2"
         ]
     }
+],
+{
+    enunciado: "Pergunta 3",
+    alternativas: [
+        "Alternativa 1",
+        "Alternativa 2"
+    ]
+},
+{
+    enunciado: "Pergunta 4",
+    alternativas: [
+        "Alternativa 1",
+        "Alternativa 2"
+    ]
+},
+{
+    enunciado: "Pergunta 5",
+    alternativas: [
+        "Alternativa 1",
+        "Alternativa 2"
+    ]
+}
 ]; 
