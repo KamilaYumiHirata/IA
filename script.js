ee1bb441112b747b42587126101917d289a42176
KamiYumi <111126080+KamilaYumiHirata@users.noreply.github.com>
2024-06-26 20:37:31 -0300

    aaaaa

--- script.js
+++ script.js
@@ -6,67 +6,67 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Qual a porcentagem que você considera relacionado ao seu sonhecimento sobre batatas?",
         alternativas: [
             {
-                texto: "Isso é assustador!",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+                texto: "De 0% a 50%",
+                afirmacao: "Uma resposta considerável sobre seu conhecimento, pronto(a) para seu resultado?"
             },
             {
-                texto: "Isso é maravilhoso!",
-                afirmacao: "Quis saber como usar IA no seu dia a dia."
+                texto: "De 50% a 100%",
+                afirmacao: "Começou bem, está pronto(a) para saber seu resultado?"
             }
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Quantos estilos de batatas você acha que existem?",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+                texto: "5 estilos apenas",
+                afirmacao: "Assim como ovos, batatas tem muitas variantes em seu nome, seria difícil concluir que ela tenha 5 estilos diferentes apenas (errou)."
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+                texto: "5 ou mais estilos",
+                afirmacao: "Batatas, sua variedade é visível, para amantes desse legume delicioso não se pode faltar pelo menos 1 tipo de variável na alimentação, seja cozida,frita, em chips,entre muitos outros (acertou)."
             }
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "O que compõe a batata?",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "É uma boa fonte de vitamina C e de algumas vitaminas do complexo B, niacina, tiamina e vitamina B6",
+                afirmacao: "Sim! a batata é rica em vitamina C e vitaminas do complexo B, principalmente niacina no qual dentre os alimentos energéticos é o mais rico, tiamina e vitamina B6, é uma fonte de ferro, bem como fósforo e magnésio além de ótima fonte de potássio (acertou)."
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                texto: "É uma boa fonte de vitamina A e de algumas vitaminas do complexo E, Riboflavina e Folato",
+                afirmacao: "A batata é rica em vitamina C e algumas outras vitaminas do complexo B, principalmente niacina no qual dentre os alimentos energéticos é o mais rico, tiamina e vitamina B6, além de muitos outros benefícios, saber sobre as vitaminas da sua alimentação do dia a dia é essencial para a saúde, mais sorte na próxima (errou)."
             }
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "Como a batata chegou ao Brasil?",
         alternativas: [
             {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+                texto: "Através dos portugueses, os colonizadores que conquistavam a região sul encontraram condições favoráveis à sua produção, fazendo com que a batata fosse uma alimentação primária para os colonos até o início do Século XX.",
+                afirmacao: "Curiosidade, quem realmente trouxe as batatas para o Brasil foram os espanhóis que conquistando o sul acharam condições favoráveis de produção para esse alimento, tornando-o a alimentação primária da época que durou até o final do século XIX (errou)."
             },
             {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+                texto: "Através dos espanhóis, os colonizadores que conquistavam a região sul encontraram condições favoráveis à sua produção, fazendo com que a batata fosse uma alimentação primária para os colonos até o final do Século XIX.",
+                afirmacao: "Mesmo que o berço das batatas esteja bem ali no Chile, quem realmente trouxe esse alimento até nosso país foram os espanhóis que conquistando o sul acharam condições favoráveis de produção para esse alimento, tornando-o a alimentação primária da época (acertou)."
             }
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "Qual o tempo de cozimento da batata?",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+                texto: "De 20 a 50 minutos dependendo do aparelho e jeito que se cozinha.",
+                afirmacao: "O cozimento das batatas na maior parte das vezes é demorado, porém tudo depende da temperatura do aparelho utilizado, alguns pode cozir esse legume em até 15 minutos (errou)."
             },
             {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+                texto: "De 10 a 40 minutos dependendo do aparelho e jeito que se cozinha.",
+                afirmacao: "O cozimento das batatas pode ser um processo demorado, porém, na temperatura certa podem levar apenas 15 minutos, geralmente chegando no máximo na maioria das vezes em até 40 minutos (acertou)."
             }
         ]
     },
@@ -105,7 +105,7 @@ function respostaSelecionada(opcaoSelecionada) {
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = "Seu resultado...";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
 }
